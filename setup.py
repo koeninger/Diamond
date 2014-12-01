f7eca4239bfec4d233d64130e8a91b27d1a1558d
cody koeninger <cody@koeninger.org>
2014-12-01 19:17:04 +0000

    make rpm work on Amazon linux distro

--- setup.py
+++ setup.py
@@ -43,8 +43,8 @@ else:
         ('share/diamond/user_scripts', []),
     ]
 
-    distro = platform.dist()[0]
-    distro_major_version = platform.dist()[1].split('.')[0]
+    distro = platform.dist(supported_dists=['system'])[0]
+    distro_major_version = platform.dist(supported_dists=['system'])[1].split('.')[0]
 
     if running_under_virtualenv():
         data_files.append(('etc/diamond',
@@ -64,7 +64,7 @@ else:
         if distro == 'Ubuntu':
             data_files.append(('/etc/init',
                                ['debian/upstart/diamond.conf']))
-        if distro in ['centos', 'redhat', 'debian', 'fedora']:
+        if distro in ['centos', 'redhat', 'debian', 'fedora', 'system']:
             data_files.append(('/etc/init.d',
                                ['bin/init.d/diamond']))
             data_files.append(('/var/log/diamond',
